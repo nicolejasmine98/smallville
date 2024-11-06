5cb7cbf3e496f733c473f31672554c7a3d6870c6
nicolejasmine98 <nrgonzales@up.edu.ph>
2024-11-06 23:02:48 +0800

    Update urls.py -> changed address of stud1info for parents

--- actualpages/dashboard/dashboard/urls.py
+++ actualpages/dashboard/dashboard/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('assignSched',index.teachclasssched, name='teachclasssched'),
     path('studentList',index.teachstudlist, name='teachstudlist'),
     path('uploadPay',index.uploadpay, name='uploadpay'),
-    path('studentInfo', index.stud1info, name='stud1info'),
+    path('studentInfoParent', index.stud1info, name='stud1info'),
 ]
